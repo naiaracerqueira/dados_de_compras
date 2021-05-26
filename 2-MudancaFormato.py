9fb75f42d8a9d362bbf61259f901fa36c5c8450c
Naiara <crqr.naiara@gmail.com>
2021-05-26 17:41:37 -0300

    comentários adicionais

--- 2-MudancaFormato.py
+++ 2-MudancaFormato.py
@@ -1,32 +1,47 @@
 import pandas as pd
 import json
 
+# Considerando que seria um projeto de Big Data, o ideal seria ter utilizado spark
+# E, considerando esse volume maior, o ideal seria enviar esses dados para um banco de dados relacional
+
+# Outro ponto de melhora seria na transformação das tabelas fato e dimensão, não acredito que essa seja a melhor solução disponível
+# (Ex: acabei tendo que fazer um drop da coluna ItemList)
+
 dataset = pd.read_json("data.json")
 print(dataset.head())
 # A coluna de ItemList possui dicionário, vou verificar qual o formato do dicionário:
-print(dataset['ItemList'][0])
+print("\nVerificar um dicionário:",dataset['ItemList'][0])
 #'ProductName','Value','Quantity'
 
 def um_dataframe():
+    '''
+    Cria um dataframe unificado, com as colunas do ItemList
+    '''
     with open('data.json','r') as f:
         data = json.loads(f.read())
     df = pd.json_normalize(data, 'ItemList', ['CreateDate','EmissionDate','Discount','NFeNumber','NFeID'], record_prefix='ItemList_')
     df = df[['CreateDate','EmissionDate','Discount','NFeNumber','NFeID','ItemList_ProductName','ItemList_Value','ItemList_Quantity']]
-    print(df.head())
+    print("\n",df.head())
     return df
 
 def tabela_fato():
+    '''
+    Cria a tabela fato.
+    '''
     fato = pd.read_json("data.json")
     fato.drop(columns='ItemList', inplace=True)
-    print(fato.head())
+    print("\n",fato.head())
     return fato
 
 def tabela_dimensao():
+    '''
+    Cria a tabela dimensão com detalhamento dos itens contidos na nota fiscal.
+    É importante considerar que a chave é a coluna de NFeID.
+    '''
     with open('data.json','r') as f:
         data = json.loads(f.read())
     dim_itens = pd.json_normalize(data, 'ItemList', ['NFeID'])
-    print(dim_itens.head())
-    # A chave é a coluna de NFeID!!!
+    print("\n",dim_itens.head())
     return dim_itens
 
 
